985c917165e75b5de0623cd511cc3c59eb314ec9
phlst <hajdu2024@gmail>
2025-05-06 21:16:36 +0200

    fixed something

--- internal/store/database.go
+++ internal/store/database.go
@@ -8,7 +8,7 @@ import (
 )
 
 func Open() (*sql.DB, error) {
-	db, err := sql.Open("pgx", "host=localhost user=postgres password=postgres dbname=postgres port=5432 sslmode=disable")
+	db, err := sql.Open("pgx", "")
 	if err != nil {
 		return nil, fmt.Errorf("db: open %w", err)
 	}
